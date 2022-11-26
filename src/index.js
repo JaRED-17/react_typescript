834d8b09a5baa6cbbb633af5c7770b412161cf67
Igor Borozdov <melnikow_igor@mail.ru>
2022-11-26 21:15:27 +0600

    Added App component

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
 import { render } from 'react-dom';
-import HelloWorld from './components/HelloWorld';
+import App from './components/App/App';
 
-render(<HelloWorld />, document.getElementById('root'));
+render(<App />, document.getElementById('root'));
