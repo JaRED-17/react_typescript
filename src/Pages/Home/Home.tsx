67bc230a42bfb0eed67ceb048c9e1976dc6931d5
Igor Borozdov <melnikow_igor@mail.ru>
2022-11-27 13:09:58 +0600

    Updated view of goods list

--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -1,17 +1,34 @@
 import React from 'react';
+import './Home.scss'
 
 interface Goods {
     name: string
+    image: string
 }
 
 const Home: React.FC = () => {
-    const goods: Array<Goods> = [{ name: "1" }, { name: "2" }];
+    const goods: Array<Goods> = [
+        { name: "1", image: "./goods.jpg" },
+        { name: "2", image: "./goods.jpg" },
+        { name: "3", image: "./goods.jpg" },
+        { name: "4", image: "./goods.jpg" },
+        { name: "5", image: "./goods.jpg" }
+    ];
     const classPrefix = 'home-page'
 
     return (
         <div className={classPrefix}>
             <div className={`${classPrefix}__goods`}>
-                {goods.map(item => <div className={`${classPrefix}__goods-item`}>{item.name}</div>)}
+                {goods.map(item => {
+                    return (
+                        <div className={`${classPrefix}__goods-item`}>
+                            <div className={`${classPrefix}__goods-item-image`}>
+                                <img src={item.image} alt="" />
+                            </div>
+                            <div className={`${classPrefix}__goods-item-name`}>{item.name}</div>
+                        </div>
+                    )
+                })}
             </div>
         </div>
     );
