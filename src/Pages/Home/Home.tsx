33b40f94c0f821b3f24bc446dfd4373d1db1e41d
Igor Borozdov <melnikow_igor@mail.ru>
2022-11-27 12:51:46 +0600

    Added goods

--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -1,9 +1,18 @@
 import React from 'react';
 
+interface Goods {
+    name: string
+}
+
 const Home: React.FC = () => {
+    const goods: Array<Goods> = [{ name: "1" }, { name: "2" }];
+    const classPrefix = 'home-page'
+
     return (
-        <div className='home-page'>
-            <h3>Home</h3>
+        <div className={classPrefix}>
+            <div className={`${classPrefix}__goods`}>
+                {goods.map(item => <div className={`${classPrefix}__goods-item`}>{item.name}</div>)}
+            </div>
         </div>
     );
 };
