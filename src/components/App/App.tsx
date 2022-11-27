0ade22c84e5cd7d4f587602768dedd19cdd7d9ef
Igor Borozdov <melnikow_igor@mail.ru>
2022-11-27 12:10:41 +0600

    Added Content component

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,18 +1,13 @@
 import React from 'react';
 import Header from "../Header/Header";
 import Footer from "../Footer/Footer";
-import Home from "../../Pages/Home/Home"
-import AboutUs from "../../Pages/AboutUs/AboutUs"
-import {Route, Switch} from "react-router-dom";
+import Content from "../Content/Content";
 
 const App: React.FC = () => {
     return (
         <>
             <Header />
-            <Switch>
-                <Route path='/' component={Home} exact />
-                <Route path='/about-us' component={AboutUs} exact />
-            </Switch>
+            <Content />
             <Footer />
         </>
     );
---
+++ src/components/Content/Content.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import {Route, Switch} from "react-router-dom";
+
+/* Pages */
+import Home from "../../Pages/Home/Home"
+import AboutUs from "../../Pages/AboutUs/AboutUs"
+
+const Content: React.FC = () => {
+  return (
+    <Switch>
+      <Route path='/' component={Home} exact />
+      <Route path='/about-us' component={AboutUs} exact />
+    </Switch>
+  );
+};
+
+export default Content;
