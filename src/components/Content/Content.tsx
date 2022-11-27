3668f3ba45b398100d99ff74a68ac579074d20f3
Igor Borozdov <melnikow_igor@mail.ru>
2022-11-27 12:11:37 +0600

    Updated Content component

--- src/components/Content/Content.tsx
+++ src/components/Content/Content.tsx
@@ -6,12 +6,12 @@ import Home from "../../Pages/Home/Home"
 import AboutUs from "../../Pages/AboutUs/AboutUs"
 
 const Content: React.FC = () => {
-  return (
-    <Switch>
-      <Route path='/' component={Home} exact />
-      <Route path='/about-us' component={AboutUs} exact />
-    </Switch>
-  );
+    return (
+        <Switch>
+            <Route path='/' component={Home} exact />
+            <Route path='/about-us' component={AboutUs} exact />
+        </Switch>
+    );
 };
 
 export default Content;
